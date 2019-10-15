33ab571505bf9f80a7b804fd14c4fe062b8ba963
Lisa5 <1348894516@qq.com>
2019-10-15 22:33:54 +0800

    menu

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -4,13 +4,13 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      "pagePath": "pages/home/home",
+      "pagePath": "../home/home",
       "text": "广场1",
       "iconPath": "../public/image/menu_1.png",
       "selectedIconPath": "../public/image/menu_1_active.png"
     },
     {
-      "pagePath": "pages/fate/fate",
+      "pagePath": "../fate/fate",
       "text": "缘分1",
       "iconPath": "../public/image/menu_2.png",
       "selectedIconPath": "../public/image/menu_2_active.png"
